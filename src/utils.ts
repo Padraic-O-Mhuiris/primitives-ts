e0849c8cd7bccbaf576c91aacb5bdd4acb7dee81
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-01-01 18:11:53 +0000

    utils docs

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,8 @@
 import { unsafeCoerce } from 'fp-ts/lib/function'
 
+/**
+ * Convenience function for simple construction of `Encoder<B, A` for branded types
+ *
+ * @since 0.0.1
+ */
 export const encodeAsUnderlying = { encode: unsafeCoerce }
